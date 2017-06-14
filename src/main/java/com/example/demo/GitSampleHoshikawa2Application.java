2151a1fe0eca9f45894578fed76233040659471a
takahiro.hoshikawa <takahiro.hoshikawa@rakus.co.jp>
2017-06-14 10:46:36 +0900

    プロジェクトを新規作成

---
+++ src/main/java/com/example/demo/GitSampleHoshikawa2Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSampleHoshikawa2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitSampleHoshikawa2Application.class, args);
+	}
+}
